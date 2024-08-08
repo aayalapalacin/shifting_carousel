2df37bc2c2e9846c37700c781b9ce0bca11ea1a3
Alex Ayala Palacin <95265085+aayalapalacin@users.noreply.github.com>
2024-08-08 18:37:27 +0000

    btn as component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ShiftingCarousel from './shiftingCarousel'
+import {ShiftingCarouselBtn} from './shiftingCarouselBtn'
 function App() {
-  const [count, setCount] = useState(0)
+  const [carouselSlide, setCarouselSlide] = useState(0);
 
   return (
     <>
-      <ShiftingCarousel />
+      <ShiftingCarousel carouselSlide={carouselSlide} />
+      <ShiftingCarouselBtn 
+        carouselSlide={carouselSlide} 
+        setCarouselSlide={setCarouselSlide}
+        />
     </>
   )
 }
--- src/shiftingCarousel.jsx
+++ src/shiftingCarousel.jsx
@@ -88,32 +88,7 @@ let carouselStylesArray =[{
   zIndex: "0",
 }
 ]
-const ShiftingCarousel = () => {
-  const [carouselSlide, setCarouselSlide] = useState(0);
-  const [enlarged, setEnlarged] = useState(false);
-
-  const handleCarouselSlideRight = () => {
-    // function to make sure carousel slide index stays within [0] and [3]
-    if (carouselSlide + 1 < 4) {
-      return carouselSlide + 1;
-    } else {
-      return 0;
-    }
-  };
-
-  const handleCarouselSlideLeft = () => {
-    // function to make sure carousel slide index stays within [0] and [-3]
-    if (carouselSlide - 1 > -4) {
-      return carouselSlide - 1;
-    } else {
-      return 0;
-    }
-  };
-
-  const handleBtnAnimation = () => {
-    setEnlarged(true);
-    setTimeout(() => setEnlarged(false), 300); // Return to normal size after 300ms
-  };
+const ShiftingCarousel = ({carouselSlide}) => {
 
 
   return (
@@ -187,34 +162,7 @@ const ShiftingCarousel = () => {
         })}
       </div>
 
-      <span className="carousel-btn-container">
-        <button
-          type="button"
-          style={{marginRight:"3rem"}}
-
-          className={`carousel-btn ${enlarged ? 'englarged2' : ''} color-sky border-sky-2 me-5`}
-          onClick={() => {
-            setCarouselSlide(handleCarouselSlideLeft());
-            handleBtnAnimation();
-          }}>
-        <i class="bi bi-arrow-left-circle-fill"></i> 
-        left
-       </button>
-      
-        <button
-          type="button"
-          className={`carousel-btn ${enlarged ? 'englarged1' : ''} color-sky border-sky-2 `}
-          onClick={() => {
-            setCarouselSlide(handleCarouselSlideRight())
-          
-            handleBtnAnimation();
-          }
-            }
-            >
-              right
-          <i className="fa-solid fa-arrow-right"></i>
-        </button>
-      </span>
+    
     </div>
   );
 };
